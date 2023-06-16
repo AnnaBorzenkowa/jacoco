0f55cf37ea177950a1457916311cf7f6f68fcf7c
Anna Borzenkowa <crownedjocker@gmail.com>
2023-06-16 15:57:29 +0300

    added test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,4 +18,16 @@ class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInAll() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 78, 8, 6, 11, 11, 12};
+        long expected = 78;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
